f25c1a2f56bc403184077f9c2afc45b730f09c47
Naser <nasersoftcorporation2013@gmail.com>
2020-03-11 17:40:28 +0600

    initial express app

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
