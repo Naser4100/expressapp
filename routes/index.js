7f82959d8bf0dd995097ff012adac3f73ccc8b8a
Naser <nasersoftcorporation2013@gmail.com>
2020-03-11 17:58:39 +0600

    user route added

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+const users = require('./users');
+
+router.get('/', (req, res) => {
+  res.send('Index Route');
+});
+
+router.get('/users', (req, res) => {
+  res.status(200).json({
+    data : [{
+      username: 'sohel123',
+    }, {
+      username: 'user123',
+    }],
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const index = require('./routes/index');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+
+// Mount Router
+app.use('/api', index);
 
 const PORT = process.env.PORT || 5000;
 
